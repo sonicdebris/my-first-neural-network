4a099289dd41b2f71162093dc82d1643ebdff808
Athos Bacchiocchi <athos.bacchiocchi@gmail.com>
2019-01-30 23:43:01 +0800

    add koma dependecy, test plotting

--- src/main/kotlin/sonic/debris/neural/network/Main.kt
+++ src/main/kotlin/sonic/debris/neural/network/Main.kt
@@ -1,7 +1,12 @@
 package sonic.debris.neural.network
 
+import koma.cumsum
+import koma.randn
 import loadDigitImages
 import java.io.File
+
+import koma.*
+import koma.extensions.*
 
 fun main(args: Array<String>) {
     println("load images...")
@@ -15,4 +20,21 @@ fun main(args: Array<String>) {
     (0..20).forEach {
         images.random().writeToPng(dest, it.toString())
     }
+
+    val a = randn(100,2)
+    val b = cumsum(a)
+
+    figure(1)
+    // Second parameter is color
+    plot(a, 'b', "First Run")
+    plot(a+1, 'y', "First Run Offset")
+    xlabel("Time (s)")
+    ylabel("Magnitude")
+    title("White Noise")
+
+    figure(2)
+    plot(b, 'g') // green
+    xlabel("Velocity (lightweeks/minute)")
+    ylabel("Intelligence")
+    title("Random Walk")
 }
