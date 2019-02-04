2f1287711e8c1dfa78733a45a548d00d4b9eaca9
Athos Bacchiocchi <athos.bacchiocchi@gmail.com>
2019-02-04 20:42:02 +0800

    add koma dependecy, test plotting

--- src/main/kotlin/sonic/debris/neural/network/Main.kt
+++ src/main/kotlin/sonic/debris/neural/network/Main.kt
@@ -1,12 +1,12 @@
 package sonic.debris.neural.network
 
-import koma.cumsum
 import koma.randn
 import loadDigitImages
 import java.io.File
 
 import koma.*
 import koma.extensions.*
+import koma.matrix.Matrix
 
 fun main(args: Array<String>) {
     println("load images...")
@@ -21,20 +21,48 @@ fun main(args: Array<String>) {
         images.random().writeToPng(dest, it.toString())
     }
 
-    val a = randn(100,2)
-    val b = cumsum(a)
 
-    figure(1)
-    // Second parameter is color
-    plot(a, 'b', "First Run")
-    plot(a+1, 'y', "First Run Offset")
-    xlabel("Time (s)")
-    ylabel("Magnitude")
-    title("White Noise")
+//    val a = randn(100,2)
+//    val b = cumsum(a)
+//
+//    figure(1)
+//    // Second parameter is color
+//    plot(a, 'b', "First Run")
+//    plot(a+1, 'y', "First Run Offset")
+//    xlabel("Time (s)")
+//    ylabel("Magnitude")
+//    title("White Noise")
+//
+//    figure(2)
+//    plot(b, 'g') // green
+//    xlabel("Velocity (lightweeks/minute)")
+//    ylabel("Intelligence")
+//    title("Random Walk")
 
-    figure(2)
-    plot(b, 'g') // green
-    xlabel("Velocity (lightweeks/minute)")
-    ylabel("Intelligence")
-    title("Random Walk")
+    val x = randn(1, 5)
+    println(x)
+
+    val y = mat[1, 2, 3 end 2, 4, 6 end 3, 6, 9]
+    println(y)
+    println(y[1..2, 1..2])
+
+    val z = Matrix(3,3) { row, col -> (col + 1.0)*(row + 1.0) }
+    println(z)
+
+    val w = y * z
+    val ew = y emul z
+    println(w)
+    println(ew)
+
+    val v = mat[1.0, 1.0, 1.0]
+    val r = v * y
+    println(r)
+
+    val v2 = mat[1.0, 2.0, 3.0]
+    val r2 = v * v2.T
+    println(r2)
+
+    val er2 = v emul v2
+    println(er2)
+
 }
